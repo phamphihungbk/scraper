9fd9e9097a2515834655ca704fa51105e6c9d9e7
Hung Pham <phamphihungbk@gmail.com>
2022-01-20 13:25:19 +0700

    chore: add browser's locale option

--- src/Facebook_Scrape_V3.py
+++ src/Facebook_Scrape_V3.py
@@ -4,23 +4,33 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import re
 
-writer = pd.ExcelWriter('../results/Facebook_Result.xlsx', engine='xlsxwriter')  # name of the output file
-xls = pd.ExcelFile('Social_Media_URLs.xlsx')  # name of the source file
-countries = ['MY', 'ID', 'PH', 'SG', 'VN', 'TH']  # sheets of the source file
+# name of the output file
+writer = pd.ExcelWriter('../results/Facebook_Result.xlsx', engine='xlsxwriter')
+# name of the source file
+xls = pd.ExcelFile('Social_Media_URLs.xlsx')
+# sheets of the source file
+countries = ['MY', 'ID', 'PH', 'SG', 'VN', 'TH']
 
 username = []
 bad_urls = []
-browser_options = Options()
-browser_options.add_argument('--headless')
-browser = webdriver.Remote('http://selenium-chrome:4444/wd/hub', options=browser_options)
+bs_options = Options()
+bs_options.add_argument('--headless')
+bs_options.add_argument('--lang=en')
+browser = webdriver.Remote(
+	'http://selenium-chrome:4444/wd/hub',
+	options=bs_options
+)
 
-browser.get("http://www.facebook.com")
-usr = browser.find_element_by_id("email")
-password = browser.find_element_by_id("pass")
-submit = browser.find_element_by_name("login")
-usr.send_keys("")
-password.send_keys("")
+browser.get('http://www.facebook.com')
+usr = browser.find_element_by_id('email')
+password = browser.find_element_by_id('pass')
+submit = browser.find_element_by_name('login')
+
+# need to put your facebook account to login
+usr.send_keys('')
+password.send_keys('')
 submit.click()
+
 # wait for login in
 time.sleep(5)
 
@@ -37,9 +47,9 @@ for country in countries:
             company = str(df1.iloc[i]['Facebook']).split("/")
             company = company[3]
             url = 'https://www.facebook.com/' + company
-            time.sleep(2)
+            time.sleep(1)
             browser.get(url)
-            time.sleep(2)
+            time.sleep(1)
             m = re.search(r'([0-9,]+) people follow this</span>', str(browser.page_source))
             num_followers = m[1]
             print(num_followers)
